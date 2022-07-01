b2f939664e443e3588339860d42e903c39e9c069
Jiří Novák <jirkanovakneda11@seznam.cz>
2022-07-01 11:22:24 +0200

    permit param

--- app/controllers/ort_alarm_controller.rb
+++ app/controllers/ort_alarm_controller.rb
@@ -8,13 +8,12 @@ class OrtAlarmController < ApplicationController
   def create
     @alarm = Alarm.new(alarm_params)
     @alarm.save!
-    puts "funguje"
     render json: {status: 'ok'}
   end
 
   private
   def alarm_params
-    params.require(:alarm).permit(:ort_name, :ort_text, :ort_typ_alarm, :ort_datovapromennaid, :statuscode)
+    params.require(:alarm).permit(:ort_name, :ort_text, :ort_typ_alarm, :ort_zarizeniid, :ort_datovapromennaid, :statuscode)
   end
 end
 
